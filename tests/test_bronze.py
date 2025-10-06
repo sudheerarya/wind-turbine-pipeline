ac89a669e669dbef2409066ec6b99ec3c843f90c
sudheerarya <sudheer.aryan@gmail.com>
2025-10-06 17:02:25 +0100

    Update test_bronze.py

--- tests/test_bronze.py
+++ tests/test_bronze.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def test_bronze_columns_exist():
-    df = pd.read_csv('data/sample/data_group_1.csv')
-    for col in ["timestamp","turbine_id","wind_speed","wind_direction","power_mw"]:
+    df = pd.read_csv('/Volumes/main/turbine/data/data_group_1.csv')
+    for col in ["timestamp","turbine_id","wind_speed","wind_direction","power_output"]:
         assert col in df.columns
